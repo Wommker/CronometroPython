3d3ee04b75c05b71b470f1fba2819592810e2680
William Morales <70636793+Wommker@users.noreply.github.com>
2021-01-28 22:35:10 -0400

    Update to Cronometro.py
    Previously there were two errors.
    
    -The first was that the conometer could be started more than once. Now thanks to the variable "run" it is known when the timer has already started
    
    -The second was that once the first 60 seconds of the program had passed, he did not do anything else, the minutes did not increase, he only restarted in conometro. Now this is not the case.
    
    It also included the millisecond count because if "self.root.after (1000, self.tick)" was used, that is, if it was called every second, the conometer could be stopped and I immediately started it again, and the conometer was called twice.
    
    Also included a variable that is activated if the timer needs to be reset, in this way when the "tick" method is called for the last millisecond the counter is initialized to zero.
    
    However, many things remained the same, such as the interface, the way the values are returned, and other things

--- Cronometro.py
+++ Cronometro.py
@@ -1,103 +1,98 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  6 14:16:44 2020
+Created : on Wed May  6 14:16:44 2020
+Updated and improved : 28/01/2020
 
 @author: jdros
+Updated and improved by: Woomker
 """
 
 from tkinter import *
 from tkinter.font import Font
 
-global continuar
-continuar=True 
+run = False 
+initialized = True
 
-class interfaz:
-    def iniciarCronometro(self):
-         global continuar
-         continuar=True
-         self.tick()
-    def detenerCronometro(self):
-        global continuar
-        continuar=False
-    def reiniciarCronometro(self):
-        global continuar
-        continuar= False
-        self.text.set("00:00:00")
-        self.horas.reinciarValor()
-        self.minutos.reinciarValor()
-        self.segundos.reinciarValor()
+class interface:
+    def startstopwatch(self):
+        global run
+        global initialized
+        if run == False:
+            run = True
+            initialized = False
+            self.tick()
+    def stopstopwatch(self):
+        global run
+        global initialized
+        run = False
+        initialized = False
+    def resetstopwatch(self):
+        global run
+        global initialized
+        run= False
+        initialized = True
+        self.hour=0
+        self.minute=0
+        self.second=0
+        self.millisecond=0
+        self.text.set("00:00:00:000")
+
     def tick(self):
-        global continuar
-        if(continuar):
-            self.segundos.aumentarValor()
-            if(self.segundos.obtenerValor()==59):
-                self.minutos.aumentarValor()
-            if(self.minutos.obtenerValor()==59):
-                self.horas.aumentarValor()
-            resultado=self.horas.obtenerValor()+":"+self.minutos.obtenerValor()+":"+self.segundos.obtenerValor()
-            self.text.set(resultado)
-        if(continuar):
-            self.root.after(930,self.tick)
-        
-    def  __init__(self,hora,minuto,segundo):
-        self.horas=hora
-        self.minutos=minuto
-        self.segundos=segundo
+        self.millisecond=self.passofthetime(self.millisecond)
+        if(self.millisecond == 999):
+            self.millisecond = 0
+            self.second=self.passofthetime(self.second)
+        if(self.second == 59):
+            self.second = 0
+            self.minute=self.passofthetime(self.minute)
+        if(self.minute == 59):
+            self.minute = 0
+            self.hour=self.passofthetime(self.hour)
+        result=self.getvalue(self.hour,False)+":"+self.getvalue(self.minute,False)+":"+self.getvalue(self.second,False)+":"+self.getvalue(self.millisecond,True)
+        self.text.set(result)
+        if run:
+            self.root.after(1,self.tick)
+        elif initialized:
+            self.text.set("00:00:00:000")
+
+                    
+    def  __init__(self):
+        self.hour=0
+        self.minute=0
+        self.second=0
+        self.millisecond=0
         self.root=Tk()
-        self.root.title("Cronometro")     #Ponerle el nombre a la ventana
+        self.root.title("stopwatch")    
         self.root.geometry("350x100")
         self.root.resizable(False,False)
         self.text = StringVar()
-        self.text.set("00:00:00")
+        self.text.set("00:00:00:000")
         self.myFont =Font(family="Times New Roman", size=18)
-        self.etiqueta = Label(self.root,textvariable=self.text)
-        self.etiqueta.pack()
-        self.etiqueta.configure(font=self.myFont)
-        self.btnReiniciar = Button(self.root,text="Reiniciar",fg="Blue",command= self.reiniciarCronometro).place(x=10,y=70,height=20,width=80) 
-        self.btnIniciar = Button(self.root,text="Iniciar",fg="Green",command= self.iniciarCronometro).place(x=130,y=70,height=20,width=80) 
-        self.btnParar = Button(self.root,text="Parar",fg="red",command= self.detenerCronometro).place(x=250,y=70,height=20,width=80)  
-    
-class Contador:
-        valor=0
-        limite=0
-        def __init__(self,Valor):
-            self.limite=Valor
-        def aumentarValor(self):
-            self.valor +=1
-        def obtenerValor(self):
-            if(self.valor<10):
-                return "0"+str(self.valor)
-            else :
-                return str(self.valor)
-        def reinciarValor(self):
-            self.valor=0
-class Hora(Contador):
-    def __init__(self,Valor):
-        Contador.__init__(self,Valor)
-    def aumentarValor(self):
-        if(self.valor==24):
-            self.valor=0
-        else:
-            self.valor +=1
-class Minuto(Contador):
-    def __init__(self,Valor):
-        Contador.__init__(self,Valor)
-    def aumentarValor(self):
-        if(self.valor==60):
-            self.valor=0
-        else:
-            self.valor +=1
-class Segundo(Contador):
-    def __init__(self,Valor):
-        Contador.__init__(self,Valor)
-    def aumentarValor(self):
-        if(self.valor==60):
-            self.valor=0
-        else:
-            self.valor +=1
+        self.label = Label(self.root,textvariable=self.text)
+        self.label.pack()
+        self.label.configure(font=self.myFont)
+        self.btnreset = Button(self.root,text="Reset",fg="Blue",command= self.resetstopwatch).place(x=10,y=70,height=20,width=80) 
+        self.btnstart = Button(self.root,text="Start",fg="Green",command= self.startstopwatch).place(x=130,y=70,height=20,width=80) 
+        self.btnstop = Button(self.root,text="Stop",fg="red",command= self.stopstopwatch).place(x=250,y=70,height=20,width=80)  
 
-hora=Hora(24)
-minuto=Minuto(60)
-segundo= Segundo(60)
-App=interfaz(hora,minuto,segundo)
-App.root.mainloop()  #Mantiene la ventana abierta
+    def passofthetime(self,value):
+        value +=1
+        return value
+        
+
+    def getvalue(self,value,zeros):
+        if zeros:
+            if value < 10:
+                return ("00" + str(value))
+            elif value < 100 and value >= 10:
+                return ("0" + str(value))
+            else:
+                return str(value)
+        if zeros == False:
+            if value <10:
+                return ("0" + str(value))
+            else:
+                return str(value)
+
+App=interface()
+App.root.mainloop()
